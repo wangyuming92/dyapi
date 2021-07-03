17cb658f338f56f8ce861d096bfc1832880caf0e
Mr.Wang <1256876753@qq.com>
2021-07-03 14:03:12 +0800

    开放

---
+++ src/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author     Mr.Wang
+ * @ctime:     2021/7/3 14:00
+ */
+require_once './vendor/autoload.php';
+use \wangyuming92\dyapi\Tools;
+
+Tools::test();
---
+++ src/wangyuming92/dyapi/Tools.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author     Mr.Wang
+ * @ctime:     2021/7/3 13:59
+ */
+
+
+namespace wangyuming92\dyapi;
+
+
+class Tools
+{
+    public static function test(){
+        echo 666;
+    }
+
+}
